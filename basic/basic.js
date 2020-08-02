201d9e8cb258b96028a29bc167e452dfe7931ba1
Nesar Ahmed <dev.nesar24@gmail.com>
2020-08-02 09:39:15 +0100

    Variable and Data Type

---
+++ basic/basic.js
@@ -0,0 +1,55 @@
+/**
+ *  | Variable
+ *  | It is a name for memory bytes that is eassy to reuse and remember
+ *  | var nameOfVariable = valueOfVariable;
+ */
+var fullName = "Nesar Ahmed";
+
+
+/**
+ *  | Javascript prompt function
+ *  | It's a pop up box to help us to take user input from browser window.
+ */
+ var name = prompt("What's your first name")
+ console.log("Hi " +name );
+
+
+/**
+ *  | String Data Type
+ *  | It's a text and help to write sequence of characers together
+ */
+var greeting = "Hello Nesar";
+var helloworld = 'Hello world';
+
+
+/**
+ *  | Number Data Type
+ *  | It represent integer and rational number.
+ */
+var age = 43;
+var priceOfMeat = 120.00;
+
+
+/**
+ *  | Boolean Data Type
+ *  | It controls our program by login
+ */
+var isAdmin = false;
+var isActive = true;
+
+
+/**
+ *  | Undefined Data Type
+ *  | variable is declared but not assign value to it.
+ */
+var fullName
+var favouriteFood = undefined;
+
+
+/**
+*  | Null Data Type
+*  | Representation of a no value
+*/
+var lastName = "Ahmed"
+var lastName = null;
+
